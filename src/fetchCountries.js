615f0d05420ea273cf2f904c69ce6247281b123f
Tonya-Shyva <toshash69@gmail.com>
2022-09-22 16:33:56 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,8 @@ export const fetchCountries = name => {
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
-      if (!response.ok) {
+      if (response.ok === false) {
+        // або (!response.ok)
         if (response.status === 404) {
           return [];
         }
