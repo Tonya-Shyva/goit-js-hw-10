3e248c0e5efe23d142c4bac1b572a237143735ea
Tonya-Shyva <toshash69@gmail.com>
2022-09-22 10:27:58 +0300

    installed lodash.debounse, notiflix
    create js with fetch export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const refs = {
+  searchInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  infoCountryDiv: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+
+refs.searchInput.addEventListener('input', onSerch);
+
+function onSerch(evt) {
+  const searchQuery = evt.currentTarget.elements.query.value;
+}
